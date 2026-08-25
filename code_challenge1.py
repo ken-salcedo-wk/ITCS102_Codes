900abeb2de4fae72e472c0a19bf58489ba19e4c7
Ken Salcedo <ken.salcedo.wk@gmail.com>
2026-08-25 10:44:43 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+challenge1 = "\t\t\t\t\t\t*\t\t\t\t\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\n\n\t\t\t\t*\t\t\t\t*\t\t\n\n\t\t\t*\t\t    my name is\t\t\t*\t\t\t\n\n\t\t*\t\t\t   KEN SALCEDO\t\t\t\t*\t\t\n\n\t\t\t*\t\t\t\t\t\t*\t\t\t\n\n\t\t\t\t*\t\t\t\t*\t\t\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\t\t\n\n\t\t\t\t\t\t*\t\t\t\t\t\t"
+
+print(challenge1)
