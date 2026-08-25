581425a1a2c5c8841eff9a3a9b7076091c702c87
Ken Salcedo <ken.salcedo.wk@gmail.com>
2026-08-25 11:22:09 +0800

    Add files via upload

---
+++ codechallenge2.py
@@ -0,0 +1,29 @@
+#codechallenge2
+#create/write a pythomscript that generate the following output
+
+#1000 - 3
+#500 - 0
+#200 - 2
+#100 - 0
+#50 - 0
+#20 - 1
+#10 - 1
+#5 - 0
+
+money = 3430
+money2 = 430
+money3 = 
+money4 = 
+
+deposit1 = 1000
+deposit2 = 200
+deposit3 = 20
+deposit4 = 10
+
+total1 = money // deposit1
+total2 = money2 // deposit2
+total3 = money / deposit1
+total4 = money / deposit1
+
+print("the deposited ",deposit1,"'s "," is",total1)
+print("the deposited ",deposit2,"'s "," is",total2)
