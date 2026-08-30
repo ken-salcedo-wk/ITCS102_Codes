07d96bdb69960229c86c863750cf1e27b8192685
Ken Salcedo <ken.salcedo.wk@gmail.com>
2026-08-30 22:36:54 +0800

    Delete code_challenge1.py

--- code_challenge1.py
+++
@@ -1,3 +0,0 @@
-challenge1 = "\t\t\t\t\t\t*\t\t\t\t\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\n\n\t\t\t\t*\t\t\t\t*\t\t\n\n\t\t\t*\t\t\t\t\t\t*\n\n\t\t*\t\t\t   my name is\t\t\t\t*\n\n\t*\t\t\t\t   KEN SALCEDO\t\t\t\t\t*\n\n\t\t*\t\t\t   BSIT-1A\t\t\t\t*\n\n\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t*\t\t\t\t*\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\n\n\t\t\t\t\t\t*\t\t\t\t\t\t\n"
-
-print(challenge1)
