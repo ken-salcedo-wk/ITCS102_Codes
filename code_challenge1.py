829b265e674a0de27947aef7e70e5c2c40ca2759
Ken Salcedo <ken.salcedo.wk@gmail.com>
2026-08-30 22:34:24 +0800

    Update code_challenge1.py

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,3 +1,3 @@
-challenge1 = "\t\t\t\t\t\t*\t\t\t\t\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\n\n\t\t\t\t*\t\t\t\t*\t\t\n\n\t\t\t*\t\t    my name is\t\t\t*\t\t\t\n\n\t\t*\t\t\t   KEN SALCEDO\t\t\t\t*\t\t\n\n\t\t\t*\t\t\t\t\t\t*\t\t\t\n\n\t\t\t\t*\t\t\t\t*\t\t\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\t\t\n\n\t\t\t\t\t\t*\t\t\t\t\t\t"
+challenge1 = "\t\t\t\t\t\t*\t\t\t\t\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\n\n\t\t\t\t*\t\t\t\t*\t\t\n\n\t\t\t*\t\t\t\t\t\t*\n\n\t\t*\t\t\t   my name is\t\t\t\t*\n\n\t*\t\t\t\t   KEN SALCEDO\t\t\t\t\t*\n\n\t\t*\t\t\t   BSIT-1A\t\t\t\t*\n\n\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t*\t\t\t\t*\t\t\n\n\t\t\t\t\t*\t\t*\t\t\t\n\n\t\t\t\t\t\t*\t\t\t\t\t\t\n"
 
 print(challenge1)
